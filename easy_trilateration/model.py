66386d7972cd9112e5c3499a5fca2ef590e4c038
rtgeorgiev <68688306+rtgeorgiev@users.noreply.github.com>
2024-04-17 20:58:48 +0300

    add z-coordinate for model

--- easy_trilateration/model.py
+++ easy_trilateration/model.py
@@ -1,47 +1,48 @@
 class Point:
     x: float
     y: float
+    z: float  # New attribute for z-coordinate
 
-    def __init__(self, x_init: float, y_init: float):
+    def __init__(self, x_init: float, y_init: float, z_init: float):
         self.x = x_init
         self.y = y_init
+        self.z = z_init
 
     def __repr__(self):
-        return "".join(["Point(", str(self.x), ",", str(self.y), ")"])
+        return f"Point({self.x}, {self.y}, {self.z})"
 
     def __eq__(self, other):
-        """Overrides the default implementation"""
         if isinstance(other, Point):
-            return (self.x == other.x) & (self.y == other.y)
+            return (self.x == other.x) and (self.y == other.y) and (self.z == other.z)
         return False
 
     def __hash__(self):
-        return hash(str(self))
+        return hash((self.x, self.y, self.z))
 
 
 class Circle:
     center: Point
     radius: float
 
-    def __init__(self, x: float, y: float, r: float):
-        self.center = Point(x, y)
+    def __init__(self, x: float, y: float, z: float, r: float):
+        self.center = Point(x, y, z)  # Store a 3D point as the center
         self.radius = r
 
     def __repr__(self):
-        return "".join(["Circle(", str(self.center.x), ", ", str(self.center.y), ", ", str(self.radius), ")"])
+        return f"Circle(center={self.center}, radius={self.radius})"
 
     def __eq__(self, other):
-        """Overrides the default implementation"""
         if isinstance(other, Circle):
-            return (self.center.x == other.center.x) & (self.center.y == other.center.y) & (self.radius == other.radius)
+            return (self.center == other.center) and (self.radius == other.radius)
         return False
 
 
 class Trilateration:
     sniffers: [Circle]
     result: Circle
-    meta: []
+    meta: float  # or any appropriate data type
 
-    def __init__(self, sniffers: [Circle], result: Circle = None):
+    def __init__(self, sniffers: [Circle], result: Circle = None, meta: float = None):
         self.sniffers = sniffers
         self.result = result
+        self.meta = meta
