5bdb7eb9593f1b9cd543390567a9ba38cb660281
rtgeorgiev <68688306+rtgeorgiev@users.noreply.github.com>
2024-04-23 14:19:54 +0100

    graph

--- easy_trilateration/graph.py
+++ easy_trilateration/graph.py
@@ -1,19 +1,17 @@
-import math
-
 import matplotlib.pyplot as plt
+import matplotlib.cm
 from random import randint
-
 import pandas as pd
-from matplotlib.animation import FuncAnimation
-from easy_trilateration import model
-import matplotlib
-
-from easy_trilateration.model import Point, Trilateration
+from easy_trilateration.model import Point, Circle
 
 
-def static(history: [model.Trilateration], actual: [Point] = [], ax=plt.axes()):
+def static(history, actual=None, ax=None):
+    if ax is None:
+        ax = plt.axes()
+
     x_values = []
     y_values = []
+    z_values = []
 
     sniffers = set()
     to_draw = []
@@ -21,77 +19,49 @@ def static(history: [model.Trilateration], actual: [Point] = [], ax=plt.axes()):
     conNumber = []
     radius = []
 
-    for i in range(len(history)):
-        tri: Trilateration = history[i]
+    for i, tri in enumerate(history):
         for sniffer in tri.sniffers:
             sniffers.add(sniffer.center)
         x_values.append(tri.result.center.x)
         y_values.append(tri.result.center.y)
+        z_values.append(tri.result.center.z)
 
         if i % 10 == 0:
             to_draw.append(create_circle(tri.result))
             conNumber.append(tri.meta)
             radius.append(tri.result.radius)
-            print("---")
-            print(tri.result.center.x, tri.result.center.y, tri.result.radius)
-            print(tri.meta)
-            print("---")
+
         if i == 0:
             for sniff in sniffers:
                 to_draw.append(create_point(sniff, color="red"))
 
+    if actual:
+        actual_x = [p.x for p in actual]
+        actual_y = [p.y for p in actual]
+        actual_z = [p.z for p in actual]
+        ax.plot(actual_x, actual_y, actual_z, color="green", linewidth=3)
 
-    actual_x = []
-    actual_y = []
-
-    data = {'Condition': conNumber,
-            'Radius': radius}
-
-    df = pd.DataFrame(data, columns=['Condition', 'Radius'])
-
-    df.to_csv('export_dataframe.csv', index=False, header=True)
-
-    # plt.plot(x_values, y_values, color="blue", linewidth=1)
-
-    for act in actual:
-        actual_x.append(act.x)
-        actual_y.append(act.y)
-    plt.plot(actual_x, actual_y, color="green", linewidth=3)
     draw(to_draw)
 
-
-# def animate(history: [model.Trilateration], ax=plt.axes()):
-#    new = FuncAnimation(plt.gcf(), anim, len(history), fargs=(history, ax,), repeat=False)
-# plt.show()
-#    return new
+    data = {'Condition': conNumber, 'Radius': radius}
+    df = pd.DataFrame(data, columns=['Condition', 'Radius'])
+    df.to_csv('export_dataframe.csv', index=False, header=True)
 
 
-# def anim(i, history: [model.Trilateration], ax):
-#    x_values = []
-#    y_values = []
-#    for i in range(i + 1):
-#        if i % 100 == 0:
-#            item = history[i]
-#            x_values.append(item.result.center.x)
-#            y_values.append(item.result.center.y)
-# for item in history[i].sniffers:
-# create_point(item.center)
-#    create_circle(history[i].result, target=True)
-#    ax.plot(x_values, y_values, 'blue', linestyle='--')
-
-
-def create_circle(circle: model.Circle, color=None, target=False):
+def create_circle(circle: Circle, color=None, target=False):
     if target:
         color = matplotlib.cm.jet(1000)
     elif color is None:
         color = matplotlib.cm.jet(randint(50, 100))
-    add_shape(plt.Circle((circle.center.x, circle.center.y), color=color, fill=False, zorder=1, radius=circle.radius,
+    add_shape(plt.Circle((circle.center.x, circle.center.y, circle.center.z), color=color, fill=False, zorder=1, radius=circle.radius,
                          alpha=0.8))
-    plt.scatter(circle.center.x, circle.center.y, color=color, s=100, zorder=2)
+    plt.scatter(circle.center.x, circle.center.y, circle.center.z, color=color, s=100, zorder=2)
 
 
-def create_point(point: model.Point, color=matplotlib.cm.jet(randint(0, 00))):
-    plt.scatter(int(point.x), int(point.y), color=color, s=100, zorder=2)
+
+def create_point(point: Point, color=matplotlib.cm.jet(randint(0, 100))):
+    plt.scatter(point.x, point.y, point.z, color=color, s=100, zorder=2)
+
 
 
 def add_shape(patch):
@@ -102,8 +72,8 @@ def add_shape(patch):
 
 def draw(draw_list):
     for item in draw_list:
-        if isinstance(item, model.Circle):
+        if isinstance(item, Circle):
             create_circle(item)
-        if isinstance(item, model.Point):
+        if isinstance(item, Point):
             create_point(item)
     plt.show()
