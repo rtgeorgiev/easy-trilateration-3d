c2d870a685f3917c165d07030140483f0b7d13c7
rtgeorgiev <68688306+rtgeorgiev@users.noreply.github.com>
2024-04-23 14:19:29 +0100

    Initialize point with the additional z coordinate

--- easy_trilateration/least_squares.py
+++ easy_trilateration/least_squares.py
@@ -1,9 +1,7 @@
 import numpy as np
-import numpy.linalg
 import pandas as pd
 from scipy.optimize import least_squares, OptimizeResult
-from easy_trilateration.model import *
-from numpy import linalg as LA
+from easy_trilateration.model import Circle, Trilateration 
 
 
 def solve_history_linear(history: [Trilateration]):
@@ -12,7 +10,7 @@ def solve_history_linear(history: [Trilateration]):
 
 
 def solve_history(history: [Trilateration]):
-    guess = Circle(0, 0, 0)
+    guess = Circle(0, 0, 0, 0)  # Initialize guess with 3D coordinates
     cond = []
     for item in history:
         guess = solve(item, guess)
@@ -28,40 +26,31 @@ def solve_linear(trilateration: Trilateration) -> Circle:
     return result
 
 
-cond_max = 0
-
-
-def solve(trilateration: Trilateration, guess: Circle = Circle(0, 0, 0)) -> Circle:
+def solve(trilateration: Trilateration, guess: Circle = Circle(0, 0, 0, 0)) -> Circle:
     result, meta = easy_least_squares(trilateration.sniffers, guess)
-    j = meta.jac
-    jt = meta.jac.transpose()
-    cov_matrix = pd.DataFrame((jt.dot(j)) ** -1)
+    cov_matrix = pd.DataFrame((meta.jac.transpose().dot(meta.jac)) ** -1)
     trilateration.result = result
-    eig = LA.eig(cov_matrix)[0]
+    eig = np.linalg.eig(cov_matrix)[0]
     meta = abs(max(eig) / min(eig))
-
     trilateration.meta = meta
-    # at least one zero is present or if number exceeds threshold
-    # if len(eig[eig != 0]) < 3 or meta > 2:
-    #    trilateration.meta = 3
     return result
 
 
 def rssi_to_distance(rssi, C=35.5510920, N=29.0735592, B=11.8099735):
-    return B ** (-1*(rssi + C) / N)
+    return B ** (-1 * (rssi + C) / N)
 
 
 def linear_least_squares(crls: [Circle]) -> Circle:
-    x, y = 0, 0
+    x, y, z = 0, 0, 0
     a = []
     b = []
     for circle in crls:
-        a.append([1, -2 * circle.center.x, -2 * circle.center.y])
-        b.append([circle.radius ** 2 - circle.center.x ** 2 - circle.center.y ** 2])
+        a.append([1, -2 * circle.center.x, -2 * circle.center.y, -2 * circle.center.z])
+        b.append([circle.radius ** 2 - circle.center.x ** 2 - circle.center.y ** 2 - circle.center.z ** 2])
 
     A = np.array(a)
 
-    x = np.array([x ** 2 + y ** 2, x, y])
+    x = np.array([x ** 2 + y ** 2 + z ** 2, x, y, z])
 
     np.dot(A, x)
 
@@ -69,21 +58,23 @@ def linear_least_squares(crls: [Circle]) -> Circle:
 
     x, residuals, rank, s = np.linalg.lstsq(A, B)
 
-    return Circle(x[1], x[2], 0)
+    return Circle(x[1], x[2], x[3], 0)
 
 
-def easy_least_squares(crls, guess=Circle(0, 0, 0)) -> tuple[Circle, OptimizeResult]:
-    g = (guess.center.x, guess.center.y, guess.radius)
+def easy_least_squares(trilateration: Trilateration, guess=Circle(0, 0, 0, 0)) -> tuple[Circle, OptimizeResult]:
+    crls = trilateration.sniffers  # Extract circles from Trilateration object
+    g = (guess.center.x, guess.center.y, guess.center.z, guess.radius)
     result = least_squares(equations, g, args=[crls])
 
-    xf, yf, rf = result.x
+    xf, yf, zf, rf = result.x
 
-    return Circle(xf, yf, rf), result
+    return Circle(xf, yf, zf, rf), result
+
 
 
 def equations(guess, crls: [Circle]):
     eqs = []
-    x, y, r = guess
+    x, y, z, r = guess
     for circle in crls:
-        eqs.append(((x - circle.center.x) ** 2 + (y - circle.center.y) ** 2 - (circle.radius - r) ** 2))
+        eqs.append(((x - circle.center.x) ** 2 + (y - circle.center.y) ** 2 + (z - circle.center.z) ** 2 - (circle.radius - r) ** 2))
     return eqs
